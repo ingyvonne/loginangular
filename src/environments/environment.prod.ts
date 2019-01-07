7ebcb21e8083db5da1cd61aa3e7836ce985c86ca
ingyvonne <ing.yvonne@gmail.com>
2019-01-07 07:46:12 +0100

    Update environment.prod.ts
    Agregamos firebaseConfig a environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyALv4WwlB21BjTswlnR8-0P6txy3djnL1g',
+    authDomain: 'login-a48ef.firebaseapp.com',
+    databaseURL: 'https://login-a48ef.firebaseio.com',
+    projectId: 'login-a48ef',
+    storageBucket: 'login-a48ef.appspot.com',
+    messagingSenderId: '986452221938'
+  }
 };
